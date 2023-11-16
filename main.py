1f949be2052655a3abb3aa9bd2782ffb2db3668e
juniooor <juniormodern@hotmail.com>
2023-11-15 21:16:44 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from pypdf import PdfReader
+from translate import Translator
+translator = Translator(to_lang='pt')
+
+reader = PdfReader("In The Night.pdf")
+
+for page in reader.pages:
+    text = page.extract_text()
+    linhas = text.splitlines()
+    for linha in linhas:
+        print(linha)
+        traducao = translator.translate(linha)
+        print(traducao, '\n')
+ 
+    print('LINHA outside')
+   
